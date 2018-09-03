12116e495568991bcca85a76fb6b6b6a7441471f
Jens Malmborg <jnsmalm@gmail.com>
2018-09-03 20:44:15 +0200

    Added priority to threads

--- src/proceed.js
+++ src/proceed.js
@@ -2,12 +2,23 @@ import { task, get_tasks } from "./task"
 import { thread } from "./thread"
 
 export function proceed(job) {
+  let _thread, _priority = 0
+
   task(function* () {
-    thread(function* () {
+    _thread = thread(function* () {
       let tasks = get_tasks(job)
       for (let task of tasks) {
         yield* task()
       }
     })
+    _thread.priority(_priority)
   })
+  return {
+    priority: function (value) {
+      if (_thread) {
+        _thread.priority(value)
+      }
+      _priority = value
+    }
+  }
 }
--- src/thread.js
+++ src/thread.js
@@ -10,12 +10,24 @@ export function thread(job) {
     throw new TypeError(
       "A thread only acceps a 'generator function' as an argument")
   }
-  _threads.push(job)
+  let thread = {
+    job: job, priority: 0
+  }
+  _threads.push(thread)
+  return {
+    priority: function (value) {
+      thread.priority = value
+    }
+  }
 }
 
 export function run_threads() {
+  _threads.reverse()
+  _threads.sort((a, b) => {
+    return b.priority - a.priority
+  })
   for (let i = _threads.length - 1; i >= 0; i--) {
-    if (_threads[i].next().done) {
+    if (_threads[i].job.next().done) {
       _threads.splice(i, 1)
     }
   }
