5ef75b1b82136f2e2bb3e461e51db10c529fdeb2
Jens Malmborg <jnsmalm@gmail.com>
2018-09-07 21:20:51 +0200

    Repeat will only have one way to cancel

--- src/repeat.js
+++ src/repeat.js
@@ -3,28 +3,17 @@ import { task, get_tasks } from "./task"
 export function repeat(job) {
   let _times = Number.MAX_VALUE
   let _completed = false
-  let _stop = false
   let _cancel = false
 
   task(function* () {
     for (let i = 0; i < _times; i++) {
-      if (_stop) {
+      if (_cancel) {
         _completed = true
         return
       }
       let tasks = get_tasks(job)
       for (let task of tasks) {
-        if (_cancel) {
-          _completed = true
-          return
-        }
-        for (let step of task()) {
-          if (_cancel) {
-            _completed = true
-            return
-          }
-          yield
-        }
+        yield* task()
       }
       yield
     }
