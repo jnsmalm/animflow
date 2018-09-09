7df39797b4ac96a9bfd144f5c48cb30dc8b39899
Jens Malmborg <jnsmalm@gmail.com>
2018-09-09 06:55:25 +0200

    Run new threads in same frame

--- src/thread.js
+++ src/thread.js
@@ -1,4 +1,4 @@
-let _threads = [], _id = 0
+let _old_threads = [], _new_threads = [], _id = 0
 
 export function thread(job) {
   if (typeof job !== "function") {
@@ -13,27 +13,41 @@ export function thread(job) {
   let thread = {
     job: job, priority: 0, id: _id++, cancel: false
   }
-  _threads.push(thread)
+  _new_threads.push(thread)
   return {
-    cancel: function() {
+    cancel: function () {
       thread.cancel = true
     },
     priority: function (value) {
       thread.priority = value
+      return this
     }
   }
 }
 
 export function run_threads() {
-  _threads.sort((a, b) => {
+  if (_new_threads.length > 0) {
+    for (let i = 0; i < _new_threads.length; i++) {
+      _old_threads.push(_new_threads[i])
+    }
+    _new_threads = []
+  }
+  do_threads(_old_threads)
+
+  // New threads might have been added, run those in the same frame.
+  do_threads(_new_threads)
+}
+
+function do_threads(threads) {
+  threads.sort((a, b) => {
     if (a.priority === b.priority) {
       return b.id - a.id
     }
     return b.priority - a.priority
   })
-  for (let i = _threads.length - 1; i >= 0; i--) {
-    if (_threads[i].cancel || _threads[i].job.next().done) {
-      _threads.splice(i, 1)
+  for (let i = threads.length - 1; i >= 0; i--) {
+    if (threads[i].cancel || threads[i].job.next().done) {
+      threads.splice(i, 1)
     }
   }
 }
