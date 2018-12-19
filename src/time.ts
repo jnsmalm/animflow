cfa7dfe77d054eacd9b6f479afe2dcb718da836e
Jens Malmborg <jnsmalm@gmail.com>
2018-12-19 17:22:38 +0100

    Changed how to run all threads

--- src/time.ts
+++ src/time.ts
@@ -1,5 +1,5 @@
 import { game } from "./game"
-import { run_threads } from "./thread"
+import { thread } from "./thread"
 
 export function time() {
   return _elapsed_time / 1000
@@ -12,7 +12,7 @@ function animation(time: number) {
   _elapsed_time = time - _last
   _last = time
 
-  run_threads()
+  thread.run_all()
   game.render()
 
   requestAnimationFrame(animation)
